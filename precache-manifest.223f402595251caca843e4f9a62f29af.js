8f0a695cfb106aecc202ba23827370fc9ef04bba
Ryan <chien.hsun.yu@gmail.com>
2020-07-06 18:43:56 -0700

    Updates

---
+++ precache-manifest.223f402595251caca843e4f9a62f29af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baad5758e5b63bf940de6085d7701125",
+    "url": "/simple_chat_bot/index.html"
+  },
+  {
+    "revision": "3edfaed2ceeac0ba140e",
+    "url": "/simple_chat_bot/static/css/main.fc40447b.chunk.css"
+  },
+  {
+    "revision": "f5c37a95fa748d635409",
+    "url": "/simple_chat_bot/static/js/2.5ac4cd02.chunk.js"
+  },
+  {
+    "revision": "3edfaed2ceeac0ba140e",
+    "url": "/simple_chat_bot/static/js/main.56d5b9bf.chunk.js"
+  },
+  {
+    "revision": "dafa7d71009491084311",
+    "url": "/simple_chat_bot/static/js/runtime~main.ce388700.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/simple_chat_bot/static/media/logo.ee7cd8ed.svg"
+  }
+]);
